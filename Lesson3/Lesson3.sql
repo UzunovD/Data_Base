e5a61d099fd4e0711ed2add5f63a895c0f8ff751
uzunov_d <duyz@ya.ru>
2019-12-20 01:41:15 +0400

    Домашнее задание выполнил. В файле Lesson3.sql - скрипт добвавления таблиц. В файле vk_db_creation.sql - весь скрипт создания БД vk и ДЗ по дабавлению таблиц.

---
+++ Lesson3/Lesson3.sql
@@ -0,0 +1,33 @@
+USE vk;
+
+DROP TABLE IF EXISTS artists;
+CREATE TABLE artists(
+	id SERIAL PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS music_albums;
+CREATE TABLE music_albums(
+	id SERIAL PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL,
+	artist_id BIGINT UNSIGNED,
+	`year` int UNSIGNED,
+	
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
+DROP TABLE IF EXISTS music;
+CREATE TABLE music(
+	id SERIAL PRIMARY KEY,
+	media_id BIGINT UNSIGNED NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+	artist_id BIGINT UNSIGNED,
+	album_id BIGINT UNSIGNED,
+	liryc TEXT,
+	
+	FOREIGN KEY (media_id) REFERENCES media(id),
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (album_id) REFERENCES music_albums(id),
+    
+    INDEX (`name`, artist_id)
+);
