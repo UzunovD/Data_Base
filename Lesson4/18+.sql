cff61d88f928536c9e121dceb9ce375270062ab4
uzunov_d <duyz@ya.ru>
2019-12-22 01:24:40 +0400

    ДЗ выполнено

---
+++ Lesson4/18+.sql
@@ -0,0 +1,10 @@
+/*Написать скрипт, отмечающий несовершеннолетних пользователей
+ *  как неактивных (поле is_active = true). При необходимости
+ *  предварительно добавить такое поле в таблицу profiles со 
+ * значением по умолчанию = false (или 0)*/
+USE vk;
+
+ALTER TABLE profiles ADD is_inactive BOOL DEFAULT FALSE NOT NULL;
+UPDATE profiles SET is_inactive = TRUE
+	WHERE ((YEAR(CURRENT_DATE)-YEAR(birthday))-(RIGHT(CURRENT_DATE,5)
+	<RIGHT(birthday,5))<=18);
